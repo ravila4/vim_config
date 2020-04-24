fd493f8ffded0f3d8b5ce682231f37d1ea86b7c8
Ricardo Avila <ravila@protonmail.com>
2020-04-24 15:01:55 -0600

    Add commands for ncmpcpp, pymol and bash

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 
+# bash
+ln -sf `pwd`/bash/.bash_aliases ~
+
 # vim
 ln -sf `pwd`/vim_config/.vim ~
 ln -sf `pwd`/vim_config/.vimrc ~
 
+# ncmpcpp
+ln -sf `pwd`/ncmpcpp/.ncmpcpp ~
+
+# pymol
+ln -sf `pwd`/pymol/.pymol ~
